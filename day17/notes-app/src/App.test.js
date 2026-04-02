1e1c9303a0a7e1d68147466d1373e54b59c54bd9
Navaneeth Kumar <knavaneeth865@gmail.com>
2026-04-02 12:09:02 +0530

    react-notes-app-and-test-cases

---
+++ day17/notes-app/src/App.test.js
@@ -0,0 +1,12 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from './App';
+test("add and delete note",()=>{
+  render(<App/>);
+  fireEvent.change(screen.getByPlaceholderText(/enter note/i),{
+    target:{value:"New Note"}
+  });
+  fireEvent.click(screen.getByText(/add/i));
+  expect(screen.getByText("New Note")).toBeInTheDocument();
+  fireEvent.click(screen.getByText(/delete/i));
+  expect(screen.queryByText("New Note")).not.toBeInTheDocument();
+});
---
+++ day17/notes-app/src/components/NoteForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function NoteForm({addNote}){
+const [note,setNote]=useState({
+    title:"my Note",
+    status:"created"
+});
+const handleSubmit =(e)=>{
+    e.preventDefault();
+    //if(!note.trim()) return;
+    addNote(note);
+    //setNote("");
+}
+return(
+    <form onSubmit={handleSubmit}>
+        <input placeholder="enter note"
+        value={note.title}
+        onChange={(e)=>setNote({title:e.target.value})}
+        />
+        <input value={note.status} onChange={(e)=>setNote({status:e.target.value})}
+        />
+        <button>Add</button>
+    </form>
+);
+}
+export default NoteForm;
